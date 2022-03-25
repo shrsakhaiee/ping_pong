4d743a25853d720f45fae7136aa7b4cc36ee9e05
sahar sakhaiee <100339904+shrsakhaiee@users.noreply.github.com>
2022-03-25 19:39:30 +0430

    Add files via upload

---
+++ Assets/script/Cpu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cpu : MonoBehaviour
+{
+    float speed = 30;
+    public GameObject Ball;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float Ball_y = Ball.transform.position.y;
+        float Ball_x = Ball.transform.position.x;
+        float Racket_y = transform.position.y;
+        float Racket_h = GetComponent<BoxCollider2D>().transform.localScale.y;
+        float direction;
+
+        if(Ball_x > 0)
+        {
+            if(Ball_y + Racket_h < Racket_y)
+          {
+            direction = -1;
+
+          }
+         else if(Ball_y  - Racket_h > Racket_y)
+          {
+             direction = 1;
+
+          }
+         else
+           {
+            direction = 0;
+
+           }
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, direction) * speed;
+        
+
+        }
+        
+    }
+}
---
+++ Assets/script/MoveRacket.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveRacket : MonoBehaviour
+{
+    float speed = 30;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // UP or W : 1
+        // DOWN or S : -1
+        float v = Input.GetAxisRaw("Vertical");
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, v) * speed;
+    }
+}
